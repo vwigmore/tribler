01e1b1991e937ad560c9765b7a72ca871f91f142
Martijn de Vos <martijn@code-up.nl>
2016-03-19 13:44:07 +0100

    Plyvel tests are not executed if Plyvel is not available

--- Tribler/Test/Core/test_leveldb_store_plyvel.py
+++ Tribler/Test/Core/test_leveldb_store_plyvel.py
@@ -1,13 +1,17 @@
+import sys
 from Tribler.Core.leveldbstore import get_write_batch_plyvel
 from Tribler.Test.Core.test_leveldb_store import ClockedAbstractLevelDBStore, AbstractTestLevelDBStore
 
 
-class ClockedPlyvelStore(ClockedAbstractLevelDBStore):
-    from Tribler.Core.plyveladapter import LevelDB
-    _leveldb = LevelDB
-    _writebatch = get_write_batch_plyvel
+# TODO Martijn: compile and enable Plyvel tests on Windows
+if sys.platform != "win32":
+    class TestPlyvelStore(AbstractTestLevelDBStore):
 
+        __test__ = True
 
-class TestPlyvelStore(AbstractTestLevelDBStore):
-    __test__ = True
-    _storetype = ClockedPlyvelStore
+        class ClockedPlyvelStore(ClockedAbstractLevelDBStore):
+            from Tribler.Core.plyveladapter import LevelDB
+            _leveldb = LevelDB
+            _writebatch = get_write_batch_plyvel
+
+        _storetype = ClockedPlyvelStore
